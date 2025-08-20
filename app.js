65689bed84eca6fac2948e7132f4411c2fc487c2
Shadow <henry.aspeti@gmail.com>
2025-08-19 22:18:15 -0400

    funcion agregar amigo

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let nombresAmigos= [];
+let nuevoAmigo="";
+
+function agregarAmigo(){
+    nuevoAmigo = document.getElementById("amigo").value.trim();
+    
+    if(nuevoAmigo === "" ){
+        alert('Ingrese un nombre vaslido');
+    }else{
+        nombresAmigos.push(nuevoAmigo);
+        console.log(nombresAmigos);
+        
+    }
+
+}
